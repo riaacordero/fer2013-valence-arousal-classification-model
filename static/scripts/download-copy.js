28c85bea7a355cd26c2aa1634911acec5b516e8b
Angelie <angeliebadar1@gmail.com>
2024-01-21 17:40:06 +0800

    modified the download-copy.js

--- static/scripts/download-copy.js
+++ static/scripts/download-copy.js
@@ -1,24 +1,34 @@
+const downloadButton = document.querySelector('.download-copy');
 
-document.querySelector('.download-copy').addEventListener('click', function() {
+
+//download button
+downloadButton.addEventListener('click', function() {
     let csv = [];
-        let rows = document.querySelectorAll('.table-container #log-table div');
+    let rows = document.querySelectorAll('.table-container #log-table div');
     
-        for (let i = 0; i < rows.length; i++) {
-            let row = [], cols = rows[i].querySelectorAll('div');
+    for (let i = 0; i < rows.length; i++) {
+        let row = [], cols = rows[i].querySelectorAll('div');
     
-            for (let j = 0; j < cols.length; j++) 
-                row.push(cols[j].innerText);
+        for (let j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
     
-            csv.push(row.join(','));        
-        }
+        csv.push(row.join(','));        
+    }
     
-        // Create and download the CSV file
-        let csvFile = new Blob([csv.join('\n')], {type: 'text/csv'});
-        let downloadLink = document.createElement('a');
-        downloadLink.download = 'data.csv';
-        downloadLink.href = window.URL.createObjectURL(csvFile);
-        downloadLink.style.display = 'none';
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-}); 
+    // Create and download the CSV file
+    let csvFile = new Blob([csv.join('\n')], {type: 'text/csv'});
+    let downloadLink = document.createElement('a');
+
+    // Get the video file name from the URL
+    let urlParams = new URLSearchParams(window.location.search);
+    let source = urlParams.get('source');
+    let videoName = source.substring(source.lastIndexOf('/') + 1, source.lastIndexOf('.'));
+    
+    downloadLink.download = videoName + '_data' + '.csv';
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+    downloadLink.style.display = 'none';
+    document.body.appendChild(downloadLink);
+    
+    downloadLink.click();
+});
     
